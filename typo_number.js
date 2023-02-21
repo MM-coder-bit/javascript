68249571615c693ddd8a2c6f56a1d96467079e84
Setup Automação Mateus Quitério Marques <mateus.marques@setup.com.br>
2023-02-21 11:07:14 -0300

    código com operações matematica

---
+++ typo_number.js
@@ -0,0 +1,30 @@
+// ------- tipo number
+
+const meuNum = 1;
+const primNum = 1;
+const segNum = 2;
+
+const somaNum = primNum + segNum; // Soma
+const subNum = primNum - segNum;  // Subtrai
+const mulNum = primNum * segNum;  // multiplica
+const divNum = primNum / segNum;  // Divide
+
+// print
+console.log(somaNum);
+console.log(subNum);
+console.log(mulNum);
+console.log(divNum);
+
+// --------- tipo ponto flutuante
+const pontFlut1 = 1.5;
+const pontFlut2 = .4;
+
+const sumpontFlut = pontFlut1 + pontFlut2;
+const subpontFlut = pontFlut1 - pontFlut2;
+const mulpontFlut = pontFlut1 * pontFlut2;
+const divpontFlut = pontFlut1 / pontFlut2;
+
+console.log(sumpontFlut);
+console.log(subpontFlut);
+console.log(mulpontFlut);
+console.log(divpontFlut);
