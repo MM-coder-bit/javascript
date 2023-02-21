b9d58e35992c023674a8c1198dece4c45a10ed05
Mateus <marques18@outlook.com>
2023-02-21 20:39:12 -0300

    refatorado código, com acrescimo de um objeto em js compoeto pelo nome e preco do livro e lógica em outro js que chama para ser inspecionado

--- algoritmos de ordenação/AlgOrdenacao.js
+++ algoritmos de ordenação/AlgOrdenacao.js
@@ -1,15 +1,15 @@
-// Precos do livros
-const precosLivros = [25,15,30,50,45,20];
+const livros = require('./listaLivros') // recebe obj() do arquivo listaLivros.js
 
 let maisBarato = 0; // Valor do menor valor
 
 // 'For' resposável por varrer todo o array
-for (let atual = 0; atual < precosLivros.length; atual++) 
+for (let atual = 0; atual < livros.length; atual++) 
 {
-    if (precosLivros[atual] < precosLivros[maisBarato]) // se o valor do index atual for menor que index o ultimo index do menor valor
+    if (livros[atual].preco < livros[maisBarato].preco) // se o valor do index atual for menor que index o ultimo index do menor valor
     {
         maisBarato = atual; // atual se torna mais barato
     }    
 }
 
-console.log(`O livro mais barato custa ${precosLivros[maisBarato]}`)
+console.log(`O livro mais barato custa ${livros[maisBarato].preco}
+e seu nome é ${livros[maisBarato].titulo}`)
---
+++ algoritmos de ordenação/listaLivros.js
@@ -0,0 +1,24 @@
+// Precos do livros
+// Criado uma lista de objetos, onde este objeto é composto por um titulo em str e preco em number
+const precosLivros = [
+    {
+        titulo:'JavaScript',preco:25
+    },
+    {
+        titulo:'PHP',preco:15
+    },
+    {
+        titulo:'Java',preco:30
+    },
+    {
+        titulo:'Elixir',preco:50
+    },
+    {
+        titulo:'GO',preco:45
+    },
+    {
+        titulo:'Python',preco:20
+    }
+]; 
+
+module.exports = precosLivros; // permitir o export por outro js
