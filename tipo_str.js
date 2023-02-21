6ed8143fbd3b848cd32d71f8f7f4c5cb5304aed4
Mateus <marques18@outlook.com>
2023-02-21 11:27:24 -0300

    adicionado teste com str, inclusive com concatenação

---
+++ tipo_str.js
@@ -0,0 +1,16 @@
+// Tipo STR
+const txt1 = "Hello";
+const txt2 = 'World';
+
+const senha = "segura4321"
+const numeroStr = "2314"
+
+const citacao = "eu disse 'oi' pra teste";
+console.log(citacao);
+
+// concatenação (+)
+
+const citacaoNome = "Meu nome é ";
+const nome = "Mateus";
+
+console.log(citacaoNome + nome);
