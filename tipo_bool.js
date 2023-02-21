69e9626d4ec995c0db4115c667e3ec763a1a5cc2
Mateus <marques18@outlook.com>
2023-02-21 14:31:04 -0300

    adicionado teste com bool

---
+++ tipo_bool.js
@@ -0,0 +1,11 @@
+// tipo bool
+const priNum = 5;
+const segNum = 10;
+
+console.log(priNum === segNum);
+
+let cadastroAtivado = true;
+
+const txt1 = "Mateus";
+const txt2 = "Marques"
+console.log(txt1 === txt2);
